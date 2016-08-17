2add6654e21e89f5a02211ba612da97a5e24a58b
Khoi Le <Khoi@Khois-MacBook-Pro.local>
2016-08-17 14:55:01 -0400

    added if else statement

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.")
+}
+else if (cardThree === cardOne) {
+	alert("Sorry, try again.")
+}
+else if (cardTwo === cardOne) {
+	alert("You found a match!")
+}
+else if (cardThree === cardFour) {
+	alert("You found a match!")
+}
+else {
+	alert("Repick")
+}
